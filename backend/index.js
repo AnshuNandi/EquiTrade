309198d7c8f78fd000d7753eefbe5f6db352f337
Anshu Nandi <anshu.nandi07@gmail.com>
2026-06-28 01:31:46 +0530

    fix: update CORS to only require dashboard URL

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://equi-trade-zeta.vercel.app"],
+    origin: [
+      "http://localhost:3000", 
+      "http://localhost:3001", 
+      process.env.DASHBOARD_URL
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
